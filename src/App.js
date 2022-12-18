4406b4439179ed495e60ad1a0caf5855bdf3a30a
harry <nuteshgorde@gmail.com>
2022-12-18 20:16:47 +0530

    task 5

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import './App.css';
 import Login from './components/auth/Login';
 import SignUp from './components/auth/Sign-Up';
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import { useSelector } from 'react-redux';
+import { useSelector , useDispatch} from 'react-redux';
 import { Navigate, Route, Routes } from "react-router-dom";
 import MainPage from './components/home page/MainPage';
+import { checker } from './components/store/auth';
+import { useEffect } from 'react';
+
 
 function App() {
+
+  const dispatch = useDispatch();
+  useEffect(()=>{ dispatch(checker()) },[])
+
 const showMainPage= useSelector(state => state.authh.isAuth)
   return (
  <div>
@@ -15,6 +22,9 @@ const showMainPage= useSelector(state => state.authh.isAuth)
     <Route path='/login' element={ <Login></Login> }></Route>
     <Route path='/' element={ <SignUp></SignUp>}></Route>
   </Routes> }
+  <Routes>
+    <Route></Route>
+  </Routes>
  </div>
   );
 }
